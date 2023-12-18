bb22d8048d70c16bec03fa4cfdedd838c6c7ef5c
Federico Pellegatta <pellegatta.federico@gmail.com>
2023-12-18 10:28:58 +0100

    Implement collectMessagesBySender in reactive gRPC service

--- server-service/src/main/java/dev/federicopellegatta/serverservice/controller/MessagingReactiveGrpcController.java
+++ server-service/src/main/java/dev/federicopellegatta/serverservice/controller/MessagingReactiveGrpcController.java
@@ -1,13 +1,15 @@
 package dev.federicopellegatta.serverservice.controller;
 
-import dev.federicopellegatta.messaging.MessageRequest;
-import dev.federicopellegatta.messaging.MessageResponse;
-import dev.federicopellegatta.messaging.ReactorMessagingServiceGrpc;
+import dev.federicopellegatta.messaging.*;
 import dev.federicopellegatta.serverservice.service.MessagingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @GrpcService
 @Slf4j
@@ -20,5 +22,29 @@ public class MessagingReactiveGrpcController extends ReactorMessagingServiceGrpc
 		log.info("Received message from reactive gRCP api");
 		
 		return request.map(messagingService::sendMessage);
+	}
+	
+	@Override
+	public Mono<GroupedMessagesResponse> collectMessagesBySender(Flux<MessageRequest> request) {
+		return request
+				.groupBy(MessageRequest::getSender)
+				.flatMap(groupedFlux -> groupedFlux.collectList()
+						.map(messageRequests -> {
+							List<String> messages = messageRequests
+									.stream()
+									.map(MessageRequest::getContent)
+									.collect(Collectors.toList());
+							
+							return SenderMessagesPair.newBuilder()
+									.setSender(groupedFlux.key())
+									.addAllMessages(messages)
+									.build();
+						})
+				)
+				.collectList()
+				.map(senderMessagesPairs -> GroupedMessagesResponse.newBuilder()
+						.addAllSenderMessages(senderMessagesPairs)
+						.build())
+				.doOnError(error -> log.error("Error in collectMessagesBySender", error));
 	}
 }
